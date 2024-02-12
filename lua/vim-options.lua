0145a35c3ef283589c839b7131f33c9cab6a572b
Rahul Singh <rahuls0599@gmail.com>
2024-02-12 19:05:15 +0530

    Adding Vim Options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,11 +11,31 @@ opt.softtabstop = 4
 opt.list = true
 opt.listchars = {
 	-- space = '_',      -- Show space as ␣
-	tab = '>~',        -- Show tab as ▸
-	trail = '@',       -- Show trailing spaces as •
-	extends = '>',     -- Show end-of-line wraps as >
-	precedes = '<',    -- Show start-of-line wraps as <
+	tab = ">~",  -- Show tab as ▸
+	trail = "@", -- Show trailing spaces as •
+	extends = ">", -- Show end-of-line wraps as >
+	precedes = "<", -- Show start-of-line wraps as <
 }
 
 -- set leader key
 vim.g.mapleader = ","
+
+-- Line numbers
+opt.nu = true
+opt.relativenumber = true
+
+-- Prevent line wrapping
+opt.wrap = false
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.termguicolors = true
+
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
+
+opt.updatetime = 50
+
+opt.colorcolumn = "80"
