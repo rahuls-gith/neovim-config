6b4709de6cf98ffcb5260c50d358bba9fa28b111
Rahul Singh <rahuls0599@gmail.com>
2024-02-14 03:13:51 +0530

    All Vim Options and Remaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,11 +1,62 @@
 local opt = vim.opt
+local g = vim.g
+-- set leader key
+g.mapleader = " "
+
+-- Netrw Settings
+-- Relative Line Number for NetRW
+g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
+-- Remove banner from netrw
+g.netrw_banner = 0
+-- Open in prior window
+g.netrw_browse_split = 4
+-- Change from left splitting to right splitting
+g.netrw_altv = 1
+-- Tree style view in netrw
+g.netrw_liststyle = 3
+
+
+
+opt.title = true																-- Show title
+vim.cmd('set path+=**')															-- Search current directory recursively
+opt.syntax = "ON"
+opt.compatible = false															-- Turn off vi compatibility mode
+
+-- Line numbers
+opt.nu = true
+opt.relativenumber = true
+
+opt.mouse = 'a'																	-- Enable mouse in all modes
+opt.ignorecase = true															-- Enable case insensitive searching
+opt.smartcase = true															-- all searches are case insensitive unless there's a capital letter
+
+opt.hlsearch = true																-- Enable all highlighted search results
+opt.incsearch = true															-- Enable incremental search
+
+-- Prevent line wrapping
+opt.wrap = false
 
 -- Indenting
 opt.expandtab = false
 opt.shiftwidth = 4
 opt.smartindent = true
-opt.tabstop = 4
+opt.tabstop = 4																	-- Tab = 4 spaces
 opt.softtabstop = 4
+
+opt.fileencoding = "utf-8"														-- Encoding set to utf-8
+opt.pumheight = 10																-- number of items in popup menu
+opt.showtabline = 2																-- Always show the tab line
+opt.laststatus = 2																-- always show statusline
+opt.showcmd = true
+opt.cmdheight = 2
+opt.showmode = true
+opt.scrolloff = 10																-- Scroll page when cursor is 10 lines from top/bottom
+opt.sidescrolloff = 10															-- Scroll page when cursor is 10 spaces from left/right
+opt.completeopt = {"menuone", "noselect"}
+opt.splitbelow = true															-- split go below
+opt.splitright = true															-- vertical split go right
+opt.termguicolors = true
+vim.cmd.colorscheme('habamax')
 
 -- Tab and Space setting
 opt.list = true
@@ -17,26 +68,9 @@ opt.listchars = {
 	precedes = "<", -- Show start-of-line wraps as <
 }
 
--- set leader key
-vim.g.mapleader = " "
+vim.cmd('filetype plugin on')													-- set filetype
+vim.cmd('set wildmenu')															-- enable wildmenu
 
--- Line numbers
-opt.nu = true
-opt.relativenumber = true
-
--- Relative Line Number for NetRW
-vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
-
--- Prevent line wrapping
-opt.wrap = false
-
-opt.hlsearch = false
-opt.incsearch = true
-
-opt.termguicolors = true
-vim.cmd.colorscheme('habamax')
-
-opt.scrolloff = 8
 opt.signcolumn = "yes"
 opt.isfname:append("@-@")
 
--- lua/vim-remaps.lua
+++ lua/vim-remaps.lua
@@ -1,28 +1,73 @@
+local map = vim.keymap.set
+
 -- Access NetRW and set relative line numbers in the window
-vim.keymap.set('n', "<leader>pv", vim.cmd.Ex)
+map('n', "<leader>pv", vim.cmd.Ex)
 
 -- Move selected block of code
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Half page up and down keeps the cursor in the middle of the screen
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
 
 -- Search results using / are in the middle of the screen
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+map("x", "<leader>p", [["_dP]])
 
 -- <leader-y> to copy to system clipboard; <y> to copy to register
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+map({ "n", "v" }, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
+map({ "n", "v" }, "<leader>d", [["_d]])
 
 -- map Esc to Ctrl+c
-vim.keymap.set("i", "<C-c>", "<Esc>")
+map("i", "<C-c>", "<Esc>")
 
 -- Replace the word you are currenly on throughout the file
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Automatically close brackets, parethesis, and quotes
+
+map("i", "'", "''<left>")
+map("i", "\"", "\"\"<left>")
+map("i", "(", "()<left>")
+map("i", "[", "[]<left>")
+map("i", "{", "{}<left>")
+map("i", "{;", "{};<left><left>")
+map("i", "/*", "/**/<left><left>")
+
+-- Open netrw in 25% split in tree view
+map("n", "<leader>e", ":25Lex<CR>")			-- space+e toggles netrw tree view 
+
+
+-- buffer navigation
+map("n", "<Tab>", ":bnext <CR>")				-- Tab goes to next buffer
+map("n", "<S-Tab>", ":bprevious <CR>")			-- Shift+Tab goes to previous buffer
+map("n", "<leader>d", ":bd <CR>")				-- Space+d delets current buffer
+
+-- adjust split sizes easier
+map("n", "<C-Left>", ":vertical resize +3<CR>")		-- Control+Left resizes vertical split +
+map("n", "<C-Right>", ":vertical resize -3<CR>")	-- Control+Right resizes vertical split -
+
+-- reload config
+map("n", "<leader>r", ":source ~/.config/nvim/init.lua<CR>")	-- reload neovim config
+
+-- Tab bindings 
+map("n", "<leader>t", ":tabnew<CR>")			-- space+t creates new tab
+map("n", "<leader>x", ":tabclose<CR>")			-- space+x closes current tab
+map("n", "<leader>j", ":tabprevious<CR>")		-- space+j moves to previous tab
+map("n", "<leader>k", ":tabnext<CR>")			-- space+k moves to next tab
+
+-- easy split generation
+map("n", "<leader>v", ":vsplit")				-- space+v creates a veritcal split
+map("n", "<leader>s", ":split")					-- space+s creates a horizontal split
+
+-- easy split navigation
+map("n", "<C-h>", "<C-w>h")						-- control+h switches to left split
+map("n", "<C-l>", "<C-w>l")						-- control+l switches to right split
+map("n", "<C-j>", "<C-w>j")						-- control+j switches to bottom split
+map("n", "<C-k>", "<C-w>k")						-- control+k switches to top split
+
